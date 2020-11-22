6edc99e1766b9002f3c853b77ec6a33efc89c03c
GabrielCC163 <gbrum.rodrigues@gmail.com>
2020-11-22 12:01:59 -0300

    feat: carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,27 +1,65 @@
 import React from "react";
 import Slick from "react-slick";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 
-import mockData from "../data/mock";
+import mockData, { Movie } from "../data/mock";
 
-const Poster = (
-  { cover, title }: { cover: string; title: string },
-  index: number
-) => (
+const Poster = ({ cover, title, score }: Movie, index: number) => (
   <article key={index}>
     <img src={cover} alt={title} />
   </article>
 );
 
-const Carousel = ({ title = "Filmes em destaque", data = mockData }) => {
+interface CarouselData {
+  title?: string;
+  data?: Movie[];
+}
+
+const Carousel = ({
+  title = "Filmes em destaque",
+  data = mockData,
+}: CarouselData) => {
+  enum Direction {
+    left,
+    right,
+  }
+
+  const SlickArrow = ({
+    direction,
+    onClick,
+  }: {
+    direction: Direction;
+    onClick?: () => {};
+  }) => (
+    <button
+      type="button"
+      className={`absolute w-16 h-full z-10 bg-black bg-opacity-50 top-0 ${
+        direction ? "right-0" : "left-0"
+      }`}
+      onClick={onClick}
+    >
+      <FontAwesomeIcon
+        icon={direction ? faChevronRight : faChevronLeft}
+        size="3x"
+      />
+    </button>
+  );
+
   const options = {
     infinite: true,
     slidesToScroll: 1,
     variableWidth: true,
+    prevArrow: <SlickArrow direction={Direction.left} />,
+    nextArrow: <SlickArrow direction={Direction.right} />,
   };
 
   return (
-    <section>
-      <h2>{title}</h2>
+    <section className="relative">
+      <h2 className="relative z-10 font-bold text-2xl ml-8 mb-4">{title}</h2>
       <Slick {...options}>
         {data.map((movie, index) => Poster(movie, index))}
       </Slick>
