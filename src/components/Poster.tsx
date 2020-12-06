e5eeb6ee86fc47b6d3885e3782a2d6197aeb1604
GabrielCC163 <gbrum.rodrigues@gmail.com>
2020-12-06 20:12:44 -0300

    feat poster

--- src/components/Poster.tsx
+++ src/components/Poster.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlayCircle } from "@fortawesome/free-solid-svg-icons";
 
+import Score from "./Score";
+
 import { Movie } from "../data/mock";
+
+import "./Poster.css";
 
 const Poster = ({ cover, title, score }: Movie, index: number) => (
   <article
@@ -10,9 +14,10 @@ const Poster = ({ cover, title, score }: Movie, index: number) => (
     key={index}
   >
     <img src={cover} alt={title} />
-    <div className="absolute inset-0 w-full h-full grid place-items-center bg-black bg-opacity-75 transition-all duration-500 ease-in-out">
+    <div className="poster cursor-pointer absolute inset-0 w-full h-full py-8 px-4 grid place-items-center bg-black bg-opacity-75 transition-all duration-500 ease-in-out opacity-0">
       <FontAwesomeIcon icon={faPlayCircle} size="5x" />
       <h2 className="text-2xl">{title}</h2>
+      <Score value={score} />
     </div>
   </article>
 );
