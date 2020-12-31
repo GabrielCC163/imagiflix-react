7985f2a3aa7d2bc370073c1adfef59e94c092a8e
GabrielCC163 <gbrum.rodrigues@gmail.com>
2020-12-31 13:39:16 -0300

    feat(Hero)

---
+++ src/data/constants.tsx
@@ -0,0 +1,11 @@
+const IMAGEURL = "https://image.tmdb.org/t/p";
+const URL = "https://api.themoviedb.org/3";
+const APIKEY = "99c095ace9ccf32628ce237f0dd5ddac";
+const APISTRING = `?api_key=${APIKEY}&language=pt-BR`;
+
+export default {
+  URL,
+  IMAGEURL,
+  APIKEY,
+  APISTRING,
+};
